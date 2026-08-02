8168e811b114e89b332844268210583d1c726ebd
Jay Qi <jayqi@users.noreply.github.com>
2026-08-02 01:14:19 -0400

    Pin workflow context in tests and sync body template fixture with action.yml. Addresses #124

--- tests/new-issue-or-comment-for-label.test.js
+++ tests/new-issue-or-comment-for-label.test.js
@@ -6,9 +6,19 @@ const newIssueOrCommentForLabel = require('../src/new-issue-or-comment-for-label
 // Shallow clone original @actions/github context
 let originalContext = { ...github.context }
 
+const testOwner = "jayqi";
+const testRepo = "not-a-real-repo";
+const testLabel = "build failed";
+const testWorkflow = "my-workflow";
+const testRunNumber = 42;
+const testRunId = 1234567890;
+const testEventName = "push";
+const testRefName = "some-ref";
+const testSha = "1234567890123456789012345678901234567890";
+
+// Keep these in sync with the defaults in action.yml
 const defaultTitleTemplate = "Failed build: {{workflow}}";
-const defaultBodyTemplate = `
-GitHub Actions workflow [{{workflow}} #{{runNumber}}](https://github.com/{{repo.owner}}/{{repo.repo}}/actions/runs/{{runNumber}}) failed.
+const defaultBodyTemplate = `GitHub Actions workflow [{{workflow}} #{{runNumber}}](https://github.com/{{repo.owner}}/{{repo.repo}}/actions/runs/{{runId}}) failed.
 
 Event: {{eventName}}
 Branch: [{{refname}}](https://github.com/{{repo.owner}}/{{repo.repo}}/tree/{{refname}})
@@ -17,10 +27,37 @@ Commit: [{{sha}}](https://github.com/{{repo.owner}}/{{repo.repo}}/commit/{{sha}}
 <sup><i>Created by [jayqi/failed-build-issue-action](https://github.com/jayqi/failed-build-issue-action)</i></sup>
 `;
 
+// What the templates above must render to, given the context mocked in beforeAll
+const expectedTitle = `Failed build: ${testWorkflow}`;
+const expectedBody = `GitHub Actions workflow [${testWorkflow} #${testRunNumber}](https://github.com/${testOwner}/${testRepo}/actions/runs/${testRunId}) failed.
+
+Event: ${testEventName}
+Branch: [${testRefName}](https://github.com/${testOwner}/${testRepo}/tree/${testRefName})
+Commit: [${testSha}](https://github.com/${testOwner}/${testRepo}/commit/${testSha})
+
+<sup><i>Created by [jayqi/failed-build-issue-action](https://github.com/jayqi/failed-build-issue-action)</i></sup>
+`;
+
+// Octokit sends every list parameter as a query string, so all values are strings
+const expectedListQuery = {
+  labels: testLabel,
+  state: "open",
+  sort: "created",
+  direction: "desc",
+  per_page: "1",
+  page: "1",
+};
+
 describe("Test newIssueOrCommentForLabel", () => {
-  const testOwner = "jayqi"
-  const testRepo = "not-a-real-repo"
-  const testLabel = "build failed";
+  // Request bodies and query strings captured by the nock interceptors, so that
+  // they can be asserted on after the call. Do not assert inside the interceptor
+  // predicate -- a throwing expect() there just makes the interceptor not match,
+  // and Jest reports "no match for request" instead of a useful diff.
+  let captured;
+  const capture = (key) => (body) => {
+    captured[key] = body;
+    return true;
+  };
 
   beforeAll(() => {
     // Mock github context
@@ -30,8 +67,12 @@ describe("Test newIssueOrCommentForLabel", () => {
         repo: testRepo
       }
     })
-    github.context.ref = 'refs/heads/some-ref'
-    github.context.sha = '1234567890123456789012345678901234567890'
+    github.context.ref = `refs/heads/${testRefName}`
+    github.context.sha = testSha
+    github.context.workflow = testWorkflow
+    github.context.runNumber = testRunNumber
+    github.context.runId = testRunId
+    github.context.eventName = testEventName
 
     // Mock error/warning/info/debug
     jest.spyOn(core, 'error').mockImplementation(() => { })
@@ -39,16 +80,25 @@ describe("Test newIssueOrCommentForLabel", () => {
     jest.spyOn(core, 'info').mockImplementation(() => { })
     jest.spyOn(core, 'debug').mockImplementation(() => { })
 
+    // Any request that isn't mocked should fail loudly rather than hit the network
+    nock.disableNetConnect();
   });
 
   afterAll(() => {
+    nock.enableNetConnect();
     // Restore original @actions/github context
     Object.defineProperty(github, 'context', {
       value: originalContext,
     });
   })
 
+  beforeEach(() => {
+    captured = {};
+  });
+
   afterEach(() => {
+    // Every endpoint a test mocks should actually have been called
+    expect(nock.pendingMocks()).toEqual([]);
     nock.cleanAll();
   });
 
